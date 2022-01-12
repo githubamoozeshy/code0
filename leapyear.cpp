099472534f4454d36283651996857526c8c7bcc8
githubamoozeshy <97639343+githubamoozeshy@users.noreply.github.com>
2022-01-12 15:20:55 -0800

    Create leapyear.cpp

---
+++ leapyear.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+    
+    int year;
+    
+    cout << "Enter a year: ";
+    cin >> year;
+    
+    if( year % 4 != 0 ^ ( year % 100 == 0 && year % 400 != 0 )
+    {
+        cout << " Not a leap year!";
+    }
+    else 
+    {
+        cout << "Leap year!";
+    }
+    
+    return 0;
+}
+  
